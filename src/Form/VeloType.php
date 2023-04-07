b06b55b5b2ad3df23baf20a7b31ebad7060945ad
houssem-eddinjallouli <126072029+houssem-eddinjallouli@users.noreply.github.com>
2023-04-07 20:44:11 +0100

    hh

---
+++ src/Form/VeloType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Velo;
+use App\Entity\Categorie;
+use App\Entity\Station;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class VeloType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('prix')
+            ->add('image', FileType::class, ['required' => false])
+            ->add('qte')
+            ->add('idCategorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nomCategorie',
+            ])
+            ->add('idStation', EntityType::class, [
+                'class' => Station::class,
+                'choice_label' => 'nomStation',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Velo::class,
+        ]);
+    }
+}
