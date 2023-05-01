0335ed1e2469c7c852f5f02d2f78518dd2f75d4b
houssem-eddinjallouli <126072029+houssem-eddinjallouli@users.noreply.github.com>
2023-05-01 10:48:16 +0100

    zina / world /qr code

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Karser\Recaptcha3Bundle\KarserRecaptcha3Bundle::class => ['all' => true],
+    Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
 ];
